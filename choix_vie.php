8263784316a141c2f0682cc2af77c4b75fc9cafa
BriceBZH <brice.rubeaux@wanadoo.fr>
2023-10-31 11:36:37 +0000

    suite projet

---
+++ choix_vie.php
@@ -0,0 +1,8 @@
+<?php 
+	//header('Location: http://www.example.com/');
+	header('Location : http://www.google.com');
+	// $race = $_GET['race'];
+	//$redirection = 'choix_vie_'.$race.'.phtml';
+	// header('Location : https://www.google.com');
+	//echo $redirection;
+?>
